bde62fb1e2b938e50d121f8604d4bc6ebcba682b
Adam Khan <akhan5280@gmail.com>
2018-09-22 21:36:00 -0500

    Downloaded the pictures locally so you can switch faster

--- ex2/slideshow.js
+++ ex2/slideshow.js
@@ -1,15 +1,15 @@
 let images = [];
 let i = 0;
-images.push("https://www.dreamworksanimation.com/wp-content/uploads/2014/10/shrek-watch-shrek4-on.jpg");
-images.push("https://i.ytimg.com/vi/lhtH6Zu9VY8/maxresdefault.jpg");
-images.push("https://amp.thisisinsider.com/images/5adf9aefbd967144788b4605-750-563.jpg");
-images.push("https://www.geek.com/wp-content/uploads/2017/01/shrek-fan-art-625x350.jpg");
-images.push("https://www.slashfilm.com/wp/wp-content/images/shrek-swamp-perplexed.jpg");
-images.push("https://www.dreamworksanimation.com/wp-content/uploads/2014/10/shrek-watch-xmas-on.jpg");
-images.push("https://files.kstatecollegian.com/2018/06/Mouth-Alien-Monster-Green-Troll-Martian-Shrek-722415-1.jpg");
-images.push("http://images6.fanpop.com/image/photos/41400000/Five-nights-at-Shrek-s-shrek-41412516-1280-720.jpg");
-images.push("http://vh1.mtvnimages.com/uri/mgid:ao:image:mtv:509317?quality=0.8&format=jpg&width=980&height=551");
-images.push("https://vignette.wikia.nocookie.net/youtubepoop/images/3/32/Shrek_SFM_Compilation/revision/latest?cb=20150205025842");
+images.push("shrek1.jpg");
+images.push("shrek2.jpg");
+images.push("shrek3.jpg");
+images.push("shrek4.jpg");
+images.push("shrek5.jpg");
+images.push("shrek6.jpg");
+images.push("shrek7.jpg");
+images.push("shrek8.jpg");
+images.push("shrek9.jpg");
+images.push("shrek10.jpg");
 
 function next() {
   let currentSlide = document.getElementById('shrek');
