89ad09c62656a5930296032c3e4cf844ad28c4c2
Adam Khan <akhan5280@gmail.com>
2018-09-25 15:46:39 -0500

    Style changes for colorchanger and password validator

--- ex1/validator.js
+++ ex1/validator.js
@@ -1,4 +1,8 @@
 function validate() {
+  if (document.getElementById('text-3') != null) {
+    let element = document.getElementById('text-3');
+    element.parentNode.removeChild(element);
+  }
   let firstPassword = document.getElementById('first');
   let secondPassword = document.getElementById('second');
   if (firstPassword.value != secondPassword.value) {
@@ -8,9 +12,14 @@ function validate() {
     alert("The passwords are not at least 8 characters long");
   }
   else {
-    firstPassword.disabled = true;
-    secondPassword.disabled = true;
-    document.getElementById('button').disabled = true;
+    firstPassword.value = "";
+    secondPassword.value = "";
     document.getElementById('button').innerHTML = "Validated";
+    var newDiv = document.createElement("div");
+    newDiv.align = "center";
+    newDiv.id = "text-3";
+    newDiv.innerHTML = "Password has been validated";
+    var currentDiv = document.getElementById("grid-container");
+    document.body.insertBefore(newDiv, currentDiv);
   }
 }
--- ex4/colorchanger.js
+++ ex4/colorchanger.js
@@ -1,3 +1,42 @@
+function listen() {
+  let red = document.getElementById('red');
+  let green = document.getElementById('green');
+  let blue = document.getElementById('blue');
+  let width = document.getElementById('width');
+  let bgred = document.getElementById('bgred');
+  let bggreen = document.getElementById('bggreen');
+  let bgblue = document.getElementById('bgblue');
+
+  red.addEventListener("focus", function(){ focus("red"); });
+  green.addEventListener("focus", function(){ focus("green"); });
+  blue.addEventListener("focus", function(){ focus("blue"); });
+  width.addEventListener("focus", function(){ focus("width"); });
+  bgred.addEventListener("focus", function(){ focus("bgred"); });
+  bggreen.addEventListener("focus", function(){ focus("bggreen"); });
+  bgblue.addEventListener("focus", function(){ focus("bgblue"); });
+  red.addEventListener("mouseover", function(){ focus("red"); });
+  green.addEventListener("mouseover", function(){ focus("green"); });
+  blue.addEventListener("mouseover", function(){ focus("blue"); });
+  width.addEventListener("mouseover", function(){ focus("width"); });
+  bgred.addEventListener("mouseover", function(){ focus("bgred"); });
+  bggreen.addEventListener("mouseover", function(){ focus("bggreen"); });
+  bgblue.addEventListener("mouseover", function(){ focus("bgblue"); });
+  red.addEventListener("mouseout", function(){ blur("red"); });
+  green.addEventListener("mouseout", function(){ blur("green"); });
+  blue.addEventListener("mouseout", function(){ blur("blue"); });
+  width.addEventListener("mouseout", function(){ blur("width"); });
+  bgred.addEventListener("mouseout", function(){ blur("bgred"); });
+  bggreen.addEventListener("mouseout", function(){ blur("bggreen"); });
+  bgblue.addEventListener("mouseout", function(){ blur("bgblue"); });
+  red.addEventListener("blur", function(){ blur("red"); });
+  green.addEventListener("blur", function(){ blur("green"); });
+  blue.addEventListener("blur", function(){ blur("blue"); });
+  width.addEventListener("blur", function(){ blur("width"); });
+  bgred.addEventListener("blur", function(){ blur("bgred"); });
+  bggreen.addEventListener("blur", function(){ blur("bggreen"); });
+  bgblue.addEventListener("blur", function(){ blur("bgblue"); });
+}
+
 function change() {
   let red = document.getElementById('red');
   let green = document.getElementById('green');
@@ -7,8 +46,7 @@ function change() {
   let bggreen = document.getElementById('bggreen');
   let bgblue = document.getElementById('bgblue');
   let maintext = document.getElementById('maintext');
-  console.log(red.value);
-  if (parseInt(red.value,16) <= 255 && parseInt(green.value,16) <= 255 && parseInt(blue.value,16) <= 255) {
+  if (parseInt(red.value,16) <= 255 && parseInt(green.value,16) <= 255 && parseInt(blue.value,16) <= 255 && (red.value+green.value+blue.value).length == 6) {
     maintext.style.borderColor = red.value + green.value + blue.value;
     console.log("border changed to", red.value + green.value + blue.value,"\nnow =",maintext.style.borderColor);
   }
@@ -34,4 +72,14 @@ function reset() {
   maintext.style.borderColor = 0;
   maintext.style.backgroundColor = "white";
   maintext.style.width = 500;
+}
+
+function focus(pre) {
+  let label = document.getElementById(pre+"label");
+  label.style.backgroundColor = "#60d7f7";
+}
+
+function blur(pre) {
+  let label = document.getElementById(pre+"label");
+  label.style.backgroundColor = "#0ec0f2";
 }
