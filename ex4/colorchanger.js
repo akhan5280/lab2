98e1b85b9d000b9f7ce7fe0c81c9d2d491168883
Adam Khan <akhan5280@gmail.com>
2018-09-25 13:12:46 -0500

    Color changer style changes

--- ex4/colorchanger.js
+++ ex4/colorchanger.js
@@ -7,6 +7,7 @@ function change() {
   let bggreen = document.getElementById('bggreen');
   let bgblue = document.getElementById('bgblue');
   let maintext = document.getElementById('maintext');
+  console.log(red.value);
   if (parseInt(red.value,16) <= 255 && parseInt(green.value,16) <= 255 && parseInt(blue.value,16) <= 255) {
     maintext.style.borderColor = red.value + green.value + blue.value;
     console.log("border changed to", red.value + green.value + blue.value,"\nnow =",maintext.style.borderColor);
@@ -17,6 +18,20 @@ function change() {
   }
   if (parseInt(bgred.value,16) <= 255 && parseInt(bggreen.value,16) <= 255 && parseInt(bgblue.value,16) <= 255) {
     maintext.style.backgroundColor = bgred.value + bggreen.value + bgblue.value;
-    console.log("bg changed to", bgred.value + bggreen.value + bgblue.value,"\nnow =", maintext.style.backgroundColor)
+    console.log("bg changed to", bgred.value + bggreen.value + bgblue.value,"\nnow =", maintext.style.backgroundColor);
   }
+  red.value = "";
+  green.value = "";
+  blue.value = "";
+  width.value = "";
+  bgred.value = "";
+  bggreen.value = "";
+  bgblue.value = "";
+}
+
+function reset() {
+  let maintext = document.getElementById('maintext');
+  maintext.style.borderColor = 0;
+  maintext.style.backgroundColor = "white";
+  maintext.style.width = 500;
 }
